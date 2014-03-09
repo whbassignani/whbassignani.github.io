0093c49b8a5c906014f34294ed356de52de66774
Hawke Bassignani <hawke@hawkeward.com>
2014-03-09 18:59:52 -0400

    Theme build, continued
    Added work archive page, featured flag for putting work on the
    homepage, mobile nav open/close, and more responsive styles.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+	// CLICK: Header navigation
+	$('#header-MobileNavOpener').click(function(event) {
+		event.preventDefault();
+		
+		$('#header-Nav').toggleClass('js-opened');
+	})
 
+	// CLICK: Portfolio item, read more link
+	$('.portfolio-Item-mobileOpener').click(function(event) {
+		event.preventDefault();
+		
+		if ($(this).parent().hasClass('js-opened')) {
+			$(this).parent().removeClass('js-opened');
+			$(this).html("Read More");
+		} else {
+			$(this).parent().addClass('js-opened');
+			$(this).html("Read Less");
+		}
+	});
+	
+	$('.fluidbox').fluidbox();
+});

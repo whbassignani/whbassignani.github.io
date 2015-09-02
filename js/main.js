e954528e388e5029fa3efe0628999e9b6123c8af
William Hawke Bassignani <hawke@hawkeward.com>
2015-09-01 22:22:09 -0700

    New navigation effects updated

--- js/main.js
+++ js/main.js
@@ -1,30 +1,83 @@
 $(document).ready(function() {
   // Variables
-  $window = $(window);
-  winHeight = $window.height();
-  winWidth = $window.width();
-
   $body = $("body");
-  
   $MainNav = $("#MainNav");
-  $MobileNavOpener = $('#MobileNavOpener')
-  
-  $body.height(winHeight);
+  $MobileNavOpener = $('#MobileNavOpener');
 
-  $window.resize(function() {
-    winHeight = $window.height();
-    winWidth = $window.width();
+  var body = document.body,
+      html = document.documentElement,
+      resizeTimer;
+      
+  var mobileNavOpener = document.getElementById("MobileNavOpener");
+
+  var bodyObj = {
+    init: function() {
+      this.height = this.getBodyHeight();
+      this.width = this.getBodyWidth();
+    },
+    getBodyHeight: function() {
+      return Math.max(Math.max(body.scrollHeight, body.offsetHeight, body.clientHeight, html.scrollHeight, html.offsetHeight));
+    },
+    getBodyWidth: function() {
+      return Math.max(Math.max(body.scrollWidth, body.offsetWidth, body.clientWidth, html.scrollWidth, html.offsetWidth));
+    },
+    refreshSizes: function() {
+      this.height = this.getBodyHeight();
+      this.width = this.getBodyWidth();
+    }
+  };
+
+  var windowObj = {
+    init: function() {
+      this.height = this.getWindowHeight();
+      this.width = this.getWindowWidth();
+    },
+    getWindowHeight: function() {
+      return window.innerHeight;
+    },
+    getWindowWidth: function() {
+      return window.innerWidth;
+    },
+    refreshSizes: function() {
+      this.height = this.getWindowHeight();
+      this.width = this.getWindowWidth();
+    }
+  };
+  
+  var refreshBodyWindowSizes = function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      body.style.height = "auto";
+
+      bodyObj.refreshSizes();
+      windowObj.refreshSizes();
+      
+      body.style.height = Math.max(bodyObj.height, windowObj.height) + "px";
+    }, 50);    
+  }
+  
+  function init() {
+    bodyObj.init();
+    windowObj.init();
     
-    $body.height(winHeight);
-  });
+    // Set the body height on page load
+    body.style.height = Math.max(bodyObj.height, windowObj.height) + "px";
+    
+    // Define event listeners
+    window.addEventListener("resize", refreshBodyWindowSizes);
+  };
+  
+  init();
+  
   
 	// CLICK: Header navigation
+	// NEED TO CONVERT TO VANILLA JS
 	$MobileNavOpener.click(function(event) {
 		event.preventDefault();
 		
-		$MainNav.toggleClass('js-opened');
+		$body.toggleClass("is-nav-opened");
 		
-		if ($MainNav.hasClass('js-opened')) {
+		if ($body.hasClass("is-nav-opened")) {
   		$MobileNavOpener.html("Close");
 		} else {
   		$MobileNavOpener.html("Menu");

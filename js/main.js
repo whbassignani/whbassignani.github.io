80076c8911768f3c928ada64fc4cbe8841632a27
Hawke Bassignani <whbassignani@gmail.com>
2016-05-03 14:48:30 -0700

    JS for drawer slide-out, green bg toolbar

--- js/main.js
+++ js/main.js
@@ -1,75 +1,16 @@
 $(document).ready(function() {
   // Variables
   $body = $("body");
-  $MainNav = $("#MainNav");
-  $MobileNavOpener = $('#MobileNavOpener');
+  $wrapper = $("#wrapper");
+  $navDrawer = $("#nav-left");
+  $navDrawerOpener = $("#nav-open-btn--left");
+  $navDrawerCloser = $("#nav-close-btn--left");
+  $overlay = $("#wrapper");
 
-/*
-  var body = document.body,
-      html = document.documentElement,
-      resizeTimer;
-      
-  var mobileNavOpener = document.getElementById("MobileNavOpener");
 
-  var bodyObj = {
-    init: function() {
-      this.height = this.getBodyHeight();
-      this.width = this.getBodyWidth();
-    },
-    getBodyHeight: function() {
-      return Math.max(Math.max(body.scrollHeight, body.offsetHeight, body.clientHeight, html.scrollHeight, html.offsetHeight));
-    },
-    getBodyWidth: function() {
-      return Math.max(Math.max(body.scrollWidth, body.offsetWidth, body.clientWidth, html.scrollWidth, html.offsetWidth));
-    },
-    refreshSizes: function() {
-      this.height = this.getBodyHeight();
-      this.width = this.getBodyWidth();
-    }
-  };
-
-  var windowObj = {
-    init: function() {
-      this.height = this.getWindowHeight();
-      this.width = this.getWindowWidth();
-    },
-    getWindowHeight: function() {
-      return window.innerHeight;
-    },
-    getWindowWidth: function() {
-      return window.innerWidth;
-    },
-    refreshSizes: function() {
-      this.height = this.getWindowHeight();
-      this.width = this.getWindowWidth();
-    }
-  };
-  
-  var refreshBodyWindowSizes = function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {
-      bodyObj.refreshSizes();
-      windowObj.refreshSizes();
-      
-      var proposedNewHeight = Math.max(bodyObj.height, windowObj.height) + "px"
-      
-      // If the new height is less than the old height, use the new height
-      body.style.height = (proposedNewHeight < bodyObj.height) ? proposedNewHeight : bodyObj.height;
-    }, 50);    
-  }
-*/
   
   function init() {
-/*
-    bodyObj.init();
-    windowObj.init();
-    
-    // Set the body height on page load
-    body.style.height = Math.max(bodyObj.height, windowObj.height) + "px";
-    
-    // Define event listeners
-    window.addEventListener("resize", refreshBodyWindowSizes);
-*/
+
   };
   
   init();
@@ -77,20 +18,14 @@ $(document).ready(function() {
   
 	// CLICK: Header navigation
 	// NEED TO CONVERT TO VANILLA JS
-	$MobileNavOpener.click(function(event) {
+	// CONSOLIDATE: REFACTOR AND DRY IT
+  $navDrawerCloser.click(function (event) {
 		event.preventDefault();
-		
-		$body.toggleClass("is-nav-opened");
-		
-		if ($body.hasClass("is-nav-opened")) {
-  		$MobileNavOpener.html("Close");
-		} else {
-  		$MobileNavOpener.html("Menu");
-		}
-	})
-	
-	// Initialize Fluidbox photo viewer on modern browsers (IE9+)
-	if (!$('html').hasClass('lt-ie9')) {
-		$('.fluidbox').fluidbox();
-	}
+		$body.toggleClass("js-nav-open--left");
+	});
+
+	$navDrawerOpener.click(function (event) {
+		event.preventDefault();
+		$body.toggleClass("js-nav-open--left");
+	});
 });

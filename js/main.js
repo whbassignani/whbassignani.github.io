2f91e267af0d073d4b5a5171c81cd17f73169e28
Hawke Bassignani <whbassignani@gmail.com>
2017-09-17 19:23:48 -0700

    Added Flickity, but not working

--- js/main.js
+++ js/main.js
@@ -2,15 +2,21 @@ $(document).ready(function() {
   // Variables
   $body = $("body");
   $wrapper = $("#wrapper");
-  $navCarousel = $(".nav-carousel");
-  $navCarouselItems = $(".nav-carousel-item");
+  $headerCarousel = $(".header-carousel");
 
   // ADD FUNCTIONS TO THE INIT FUNCTION/OBJECT
   var init = function() {
-
+    $headerCarousel.flickity({
+      // Options
+      cellAlign: 'left',
+      cellSelector: ".header-carousel-item",
+      contain: true,
+      freeScroll: true,
+      prevNextButtons: false,
+      pageDots: false,
+      // wrapAround: true
+    });
   };
-
-
 
   init();
 });

f7e18d171719fd2427e3c6228c435975082edccb
William Hawke Bassignani <whbassignani@gmail.com>
2019-05-27 20:27:12 -0700

    Changed Source Sans for Concourse; added JS file; started image lazy-load work

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*
+ * Thanks to Google Developers for the lazy loading script:
+ * https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
+ */
+
+ console.log("Hey girl hey");

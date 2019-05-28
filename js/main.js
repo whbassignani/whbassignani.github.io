b3403aa146000ade2765c4eeb1a1c3ad806d9ba4
William Hawke Bassignani <whbassignani@gmail.com>
2019-05-27 22:35:32 -0700

    Added most of image lazy loading; started responsive layout work

--- js/main.js
+++ js/main.js
@@ -3,4 +3,42 @@
  * https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/
  */
 
- console.log("Hey girl hey");
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.is-notLoaded"));
+console.log(IntersectionObserverEntry.prototype);
+
+  /* WHY: need to check if full IntersectionObserver spec is available */
+  if ('IntersectionObserver' in window
+      && 'IntersectionObserverEntry' in window 
+      && 'intersectionRatio' in window.IntersectionObserverEntry.prototype
+      && 'isIntersecting' in IntersectionObserverEntry.prototype) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          // TODO: add next line back in once srcset added
+          // lazyImage.srcset = lazyImage.dataset.srcset;
+          
+          /* WHY: once image finishes loading, takes off the .is-notLoaded class, which creates the fade-in animation */
+          var newImage = new Image();
+          newImage.src = lazyImage.src;
+          newImage.onload = function() {
+            console.log("YOUSDLFKSDJFLSDKJF");
+            lazyImage.classList.remove("is-notLoaded");
+          }
+          
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    }, {
+      rootMargin: "0px 0px 300px 0px"
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+
+  }
+});
